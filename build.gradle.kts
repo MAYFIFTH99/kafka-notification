6c53b74189fa80a72b0e8a2ff79a98e071b6928b
harvey <alstjr971@naver.com>
2025-02-18 07:46:37 +0900

    Docs: SubProject 롬복, 테스트 라이브러리 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,13 @@ subprojects{
     }
 
     dependencies {
+        compileOnly("org.projectlombok:lombok")
+        annotationProcessor("org.projectlombok:lombok")
+
+
         testImplementation(platform("org.junit:junit-bom:5.10.0"))
         testImplementation("org.junit.jupiter:junit-jupiter")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
 
     tasks.test {
