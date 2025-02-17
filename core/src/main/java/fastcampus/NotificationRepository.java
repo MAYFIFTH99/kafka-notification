e8764dbd36fd76c813e12454e6352be4900e9024
harvey <alstjr971@naver.com>
2025-02-18 07:23:19 +0900

    Feat: Notification 저장 Repository
    인터페이스 생성
    추후 다양한 Impl 객체로 확장 가능

---
+++ core/src/main/java/fastcampus/NotificationRepository.java
@@ -0,0 +1,11 @@
+package fastcampus;
+
+import java.util.Optional;
+
+public interface NotificationRepository {
+    Optional<Notification> findById(String id);
+
+    Notification save(Notification notification);
+
+    Notification deleteById(String id);
+}
