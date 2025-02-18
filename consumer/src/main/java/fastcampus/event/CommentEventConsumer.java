56aeea7ab386dfcf91ce7fbce2dee270e29e7f9f
harvey <alstjr971@naver.com>
2025-02-19 01:39:45 +0900

    Feat: Comment 알림 생성 시 동작하는 이벤트 함수 구현

--- consumer/src/main/java/fastcampus/event/CommentEventConsumer.java
+++ consumer/src/main/java/fastcampus/event/CommentEventConsumer.java
@@ -1,7 +1,9 @@
 package fastcampus.event;
 
+import fastcampus.task.CommentAddTask;
 import java.util.function.Consumer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +11,16 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class CommentEventConsumer {
 
+    @Autowired
+    private CommentAddTask commentAddTask;
+
     @Bean("comment")
     public Consumer<CommentEvent> comment(){
-        return event -> log.info(event.toString());
+        return event -> {
+            if (event.getType() == CommentEventType.ADD) {
+                commentAddTask.processEvent(event);
+
+            }
+        };
     }
 }
