6965ca5172da576b8c42874297e29c80f8b2c77f
harvey <alstjr971@naver.com>
2025-02-18 09:30:37 +0900

    Feat: Mongo DB 연결 설정
    MONGO TEMPLATE 빈 생성

---
+++ core/src/main/java/fastcampus/config/MongoTemplateConfig.java
@@ -0,0 +1,29 @@
+package fastcampus.config;
+
+import static fastcampus.config.MongoTemplateConfig.MONGO_TEMPLATE;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(
+        basePackages = "fastcampus",
+        mongoTemplateRef = MONGO_TEMPLATE
+)
+
+// Mongo DB 연결
+public class MongoTemplateConfig {
+    public static final String MONGO_TEMPLATE = "notificationMongoTemplate";
+
+    @Bean(name = MONGO_TEMPLATE)
+    public MongoTemplate notificationMongoTemplate(
+        MongoDatabaseFactory notificationMongoFactory,
+            MongoConverter mongoConverter
+    ){
+        return new MongoTemplate(notificationMongoFactory, mongoConverter);
+    }
+}
