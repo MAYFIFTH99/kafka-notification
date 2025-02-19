62977ecdb4e28eb5c067b6822396fa27aac5a099
harvey <alstjr971@naver.com>
2025-02-20 08:30:12 +0900

    Test: Like Event Test API Swagger 추가

--- consumer/src/main/java/fastcampus/api/EventConsumerTestControllerSpec.java
+++ consumer/src/main/java/fastcampus/api/EventConsumerTestControllerSpec.java
@@ -1,6 +1,7 @@
 package fastcampus.api;
 
 import fastcampus.event.CommentEvent;
+import fastcampus.event.LikeEvent;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
@@ -24,12 +25,37 @@ public interface EventConsumerTestControllerSpec {
     )
     void comment(CommentEvent event);
 
+    @Operation(
+            requestBody = @RequestBody(
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(name = "게시물 좋아요 이벤트", value = LIKE_EVENT_PAYLOAD                                            )
+                                    }
+                            )
+                    }
+            )
+    )
+    void like(LikeEvent event);
+
+
+
     String COMMENT_EVENT_PAYLOAD = """
             {
                 "type": "ADD",
                 "postId": 1,
                 "userId": 2,
-                "commentId": 3
+                "commentId": 333
+            }
+            """;
+
+    String LIKE_EVENT_PAYLOAD = """
+            {
+                "type": "ADD",
+                "postId": 1,
+                "userId": 2,
+                "createdAt": "2021-08-01T00:00:00Z"
             }
             """;
 }
