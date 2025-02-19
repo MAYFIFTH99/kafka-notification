b401d5fc50daed245377f9442c4673ebb4fdb0da
harvey <alstjr971@naver.com>
2025-02-20 08:29:15 +0900

    Feat: LikeAdd Task 추가

---
+++ consumer/src/main/java/fastcampus/task/LikeAddTask.java
@@ -0,0 +1,84 @@
+package fastcampus.task;
+
+import fastcampus.LikeNotification;
+import fastcampus.Notification;
+import fastcampus.NotificationGetService;
+import fastcampus.NotificationIdGenerator;
+import fastcampus.NotificationSaveService;
+import fastcampus.NotificationType;
+import fastcampus.Post;
+import fastcampus.PostClient;
+import fastcampus.event.LikeEvent;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LikeAddTask {
+
+    @Autowired
+    private PostClient postClient;
+
+    @Autowired
+    private NotificationGetService getService;
+
+    @Autowired
+    private NotificationSaveService saveService;
+
+    public void processEvent(LikeEvent event) {
+        Post post = postClient.getPost(event.getPostId());
+        if (post == null) {
+            log.error("Post is null with postId: {}", event.getPostId());
+            return;
+        }
+        if(post.getUserId().equals(event.getUserId())){
+            return;
+        }
+
+        // like notification 1. 신규 생성, 2. 업데이트 ->  like notification db 저장
+        saveService.upsert(createOrUpdateNotification(post, event));
+    }
+
+    private Notification createOrUpdateNotification(Post post, LikeEvent event){
+        Optional<Notification> optionalNotification = getService.getNotificationByTypeAndPostId(
+                NotificationType.LIKE, post.getId());
+
+        Instant now = Instant.now();
+        Instant retention = now.plus(90, ChronoUnit.DAYS);
+
+        if(optionalNotification.isPresent()){
+            // update
+            return updateNotification((LikeNotification) optionalNotification.get(),event,now, retention);
+        }
+        else{
+            // create
+            return createNotification(post, event, now, retention);
+
+        }
+
+    }
+
+    private Notification updateNotification(LikeNotification notification, LikeEvent event, Instant now, Instant retention){
+        notification.addLiker(event.getUserId(),event.getCreatedAt(),now, retention);
+        return notification;
+    }
+
+    private Notification createNotification(Post post, LikeEvent event, Instant now, Instant retention){
+        return new LikeNotification(
+                NotificationIdGenerator.generate(),
+                post.getUserId(),
+                NotificationType.LIKE,
+                event.getCreatedAt(),
+                now,
+                now,
+                retention,
+                post.getId(),
+                List.of(event.getUserId())
+        );
+    }
+}
