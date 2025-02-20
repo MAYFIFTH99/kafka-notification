3adb063ec9564ffb404dbff0296a22d307aa8fb1
harvey <alstjr971@naver.com>
2025-02-20 19:50:41 +0900

    Docs: 신규 알림 조회 여부 API Docs

---
+++ api/src/main/java/fastcampus/controller/CheckNewNotificationControllerSpec.java
@@ -0,0 +1,14 @@
+package fastcampus.controller;
+
+import fastcampus.response.CheckNewNotificationResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "사용자 알림센터 API")
+public interface CheckNewNotificationControllerSpec {
+
+    @Operation(summary = "사용자 신규 알림 조회 여부")
+    CheckNewNotificationResponse checkNew(
+            @Parameter(example = "1") Long userId);
+}
